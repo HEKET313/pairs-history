244fec0f8efca427dfd37b8c92fb9450b4c9d2da
Nikita Leshchev <neket313@gmail.com>
2020-05-10 23:35:13 +0400

    Крон

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -3,6 +3,8 @@
 composer install
 php bin/console doctrine:migrations:migrate --no-interaction
 
+supercronic /etc/cron.d/import-job > /proc/1/fd/1 &
+php /var/www/pairs/bin/console pairs:import > /proc/1/fd/1 &
 tail -f /var/log/php.log > /proc/1/fd/1 &
 
 php-fpm -F
